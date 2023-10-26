e1663c26370d57b601e5e319e3f65d787a695277
Posp_central <nehagupta@pbpartners.com>
2023-10-26 15:58:58 +0530

    Changes in Raise mapping request

--- src/test/java/stepDef/MotorRequest/MotorRequestStepdefs.java
+++ src/test/java/stepDef/MotorRequest/MotorRequestStepdefs.java
@@ -4,16 +4,20 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static stepDef.TestBase.driver;
 import static stepDef.TestBase.prop;
 
 public class MotorRequestStepdefs {
+    WebDriver driver1;
     @When("^Click on New Motor Request$")
     public void clickOnNewMotorRequest() throws InterruptedException {
         driver.findElement(By.xpath("//a[@class='toggle-link']")).click();
@@ -53,10 +57,23 @@ public class MotorRequestStepdefs {
 
     @Then("^Choose Insurers$")
     public void chooseInsurers() throws InterruptedException {
-        WebElement dropdown = driver.findElement(By.xpath("//*[@id=\"insurer_list\"]/span/span[1]/span"));
-
+        WebElement dropdown = driver.findElement(By.xpath("//ul[@class='select2-selection__rendered']//input[@class='select2-search__field']"));
+        dropdown.sendKeys("ICICI Lombard");
+        dropdown.sendKeys(Keys.ENTER);
+        /* WebElement dropdown = driver.findElement(By.xpath("//ul[@class='select2-selection__rendered']//input[@class='select2-search__field']"));
+        dropdown.click();
         Select select = new Select(dropdown);
-        select.selectByVisibleText("//*[@id=\"select2-Insurers-result-bd3s-2\"]");
+        select.selectByVisibleText("(//li[@class='select2-results__option'])[1]");*/
+
+        // Locate the dropdown input field and click it to open the dropdown
+//        WebElement dropdown = driver.findElement(By.xpath("//ul[@class='select2-selection__rendered']//input[@class='select2-search__field']"));
+//        dropdown.click();
+//        // Wait for the dropdown options to become visible (use WebDriverWait if necessary)
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        WebElement option = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//li[@class='select2-results__option'])[1]")));
+//        // Click the desired option
+//        option.click();
+
 
 
 //        WebDriverWait wait= new WebDriverWait(driver,10);
@@ -107,14 +124,54 @@ public class MotorRequestStepdefs {
     }
     @Then("^Choose Add-ons$")
     public void chooseAddOns() {
-        driver.findElement(By.xpath(prop.getProperty("addons"))).click();
-        WebElement searchInput = driver.findElement(By.xpath("(//ul[@class='select2-selection__rendered'])[2]"));
-//        searchInput.sendKeys("Daily Allowance");
-        searchInput.click();
-        WebDriverWait wait= new WebDriverWait(driver,10);
-        WebElement SearchText =driver.findElement(By.xpath("(//li[@class='select2-results__option'])[5]"));
-        SearchText.click();
+        WebElement addons = driver.findElement(By.xpath("(//ul[@class='select2-selection__rendered']//input[@class='select2-search__field'])[2]"));
+        addons.sendKeys("Zero Dep");
+        addons.sendKeys(Keys.ENTER);
 
+    }
+
+    @Then("^Click on Request Quote button$")
+    public void clickOnRequestQuoteButton() {
+       WebElement ReqQuote= driver.findElement(By.xpath(prop.getProperty("reqQuoteButton")));
+        JavascriptExecutor jse2 = (JavascriptExecutor) driver;
+        jse2.executeScript("arguments[0].scrollIntoView()", ReqQuote);
+        jse2.executeScript("arguments[0].click();", ReqQuote);
+    }
+
+    @Then("^Fetch the Request Id$")
+    public void fetchTheRequestId() {
+        WebElement reqID = driver.findElement(By.xpath(prop.getProperty("requestID")));
+        String sentence = reqID.getText();
+        // Split the sentence into words using whitespace as the delimiter
+        String[] words = sentence.split("\\s+");
+        // Extract the last word from the array
+        String endWord = words[words.length - 1];
+        // Display the last word
+        System.out.println("Last Word: " + endWord);
+    }
+
+    @Then("^Open the Admin panel$")
+    public void openTheAdminPanel() {
+//        System.setProperty("webdriver.chrome.driver", "D:\\Cucumber_automation\\Test\\chromedriver.exe");
+        ChromeDriver driver1 = new ChromeDriver();
+        driver1.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver1.get("https://polbkqa.policybazaar.com/login");
+        driver1.manage().window().maximize();
+        driver1.findElement(By.xpath("//input[@id='login_field']")).sendKeys("Dev4");
+        driver1.findElement(By.xpath("//input[@id='password']")).sendKeys("123456@");
+        WebElement childElement=driver1.findElement(By.xpath("//button[contains(text(),'Sign In')]"));
+//        driver1.findElement(By.xpath("//button[contains(text(),'Sign In')]")).click();
+        JavascriptExecutor jse1 = (JavascriptExecutor) driver1;
+        jse1.executeScript("arguments[0].scrollIntoView()", childElement);
+        jse1.executeScript("arguments[0].click();", childElement);
+    }
+
+    @Then("^Open the Offline Request through admin$")
+    public void openTheOfflineRequestThroughAdmin() {
+        WebElement childElement = driver.findElement(By.xpath(prop.getProperty("offlineReq")));
+        JavascriptExecutor jse2 = (JavascriptExecutor) driver1;
+        jse2.executeScript("arguments[0].scrollIntoView()", childElement);
+        jse2.executeScript("arguments[0].click();", childElement);
     }
 }
 
--- src/test/resources/features/MotorRequest/MotorRequest.feature
+++ src/test/resources/features/MotorRequest/MotorRequest.feature
@@ -7,13 +7,18 @@ Feature: New Motor Request of Car
     Then Click on verify otp buttons
 
   Scenario Outline: Generate New Motor Request for Car
+#    Combination = OV + PC +Comp+PYP no
     When Click on New Motor Request
     Then Click on Vehicle details,Registration Date, Registration NO, "<registraNum>"
-    Then Choose Insurers
     Then IDV value, customer-email, Mobile no. and Pan number "<idvValue>", "<custemail>", "<mobileNo>", "<panNo>"
     Then choose DOB
     Then upload RC "<rc>"
     Then Choose Add-ons
+    Then Choose Insurers
+    Then Click on Request Quote button
+    Then Fetch the Request Id
+    Then Open the Admin panel
+    Then Open the Offline Request through admin
 
 
 
